09b322af81920386d563b37a7f53bae458703511
uthree <33905956+uthree@users.noreply.github.com>
2021-06-12 06:23:27 +0900

    added mask

--- attention.py
+++ attention.py
@@ -10,19 +10,31 @@ class Attention(nn.Module):
         self.linear = nn.Linear(input_dim*2, output_dim)
         self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, src, tgt):
+    def forward(self, src, tgt, mask): #TODO: write mask
         # src    : [batch, src_len, input_dim]
         # tgt    : [batch, tgt_len, input_dim]
-        # output : [batch, tgt_len, output_dim]
+        # mask   : [batch, src_len, tgt_len] 
+        # output : [batch, tgt_len, output_dim], [batch, tgt_len, src_len]
+
+        # transpose
         t_src = torch.transpose(src, 1, 2) # [batch, input_dim, src_len]
         weight = torch.bmm(tgt, t_src) # [batch, tgt_len, src_len]
-        print(weight.size(), weight[0])
-        weight = self.softmax(weight)
-        print(weight.size(), weight[0])
         
-        # [batch, tgt_len, input_dim]
-        weight_sum = torch.bmm(weight, src) # [input_dim, tgt_len]
+        # mask
+        mask = torch.transpose(mask, 1, 2) # [batch, tgt_len, src_len]
+        mask = mask.to(torch.float64) # convert bool -> float
+        mask *= 1e16
+        weight += mask
+
+        # softmax
+        weight = self.softmax(weight)
+        
+        weight_sum = torch.bmm(weight, src) # [batch, input_dim, tgt_len]
         output = torch.cat([weight_sum, tgt], dim=2)
         output = self.linear(output)
         return output, weight
 
+attn = Attention(20, 20)
+src, tgt = torch.randn((10, 5, 20)), torch.randn((10, 4, 20))
+mask = torch.ones((10, 5, 4))
+result = attn(src, tgt, mask)
